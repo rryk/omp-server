ba68ef55ccb3698d463786711a4631dcb998b36d
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-03-26 16:37:48 +0100

    Reformatted code with a new style (more compact and similar to Google).

--- KIARA/SerializedDataWriter.cs
+++ KIARA/SerializedDataWriter.cs
@@ -1,36 +1,33 @@
 using System;
 using System.Collections.Generic;
 
-namespace KIARA
-{
-    public class SerializedDataWriter
-    {
-        private List<byte> data = new List<byte>();
+namespace KIARA {
+  public class SerializedDataWriter {
+    private List<byte> Data = new List<byte>();
 
-        public void WriteZCString(string value) {
-            // Write characters.
-            for (int i = 0; i < value.Length; i++)
-                WriteUint16(value[i]);
+    public void WriteZCString(string value) {
+      // Write characters.
+      for (int i = 0; i < value.Length; i++)
+        WriteUint16(value[i]);
 
-            // Write closing 0.
-            WriteUint16(0);
-        }
-
-        public void WriteUint32(UInt32 value) {
-            data.AddRange(BitConverter.GetBytes(value));
-        }
-
-        public void WriteUint16(UInt16 value) {
-            data.AddRange(BitConverter.GetBytes(value));
-        }
-
-        public void WriteInt32(Int32 value) {
-            data.AddRange(BitConverter.GetBytes(value));
-        }
-
-        public byte[] ToByteArray() {
-            return data.ToArray();
-        }
+      // Write closing 0.
+      WriteUint16(0);
     }
-} 
 
+    public void WriteUint32(UInt32 value) {
+      Data.AddRange(BitConverter.GetBytes(value));
+    }
+
+    public void WriteUint16(UInt16 value) {
+      Data.AddRange(BitConverter.GetBytes(value));
+    }
+
+    public void WriteInt32(Int32 value) {
+      Data.AddRange(BitConverter.GetBytes(value));
+    }
+
+    public byte[] ToByteArray() {
+      return Data.ToArray();
+    }
+  }
+}
