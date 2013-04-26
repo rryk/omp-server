2cbedf326abd63a99e8bd6aa17fd4d37c2f05adc
Sergiy Byelozyorov <byelozyorov@cs.uni-saarland.de>
2013-04-26 19:38:21 +0200

    Fixed conversion of exceptions.

--- KIARA/ConversionUtils.cs
+++ KIARA/ConversionUtils.cs
@@ -1,5 +1,6 @@
 using System;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace KIARA
 {
@@ -7,26 +8,30 @@ namespace KIARA
     {
         public static object CastJObject(object obj, Type destType)
         {
-          if (obj == null)                                      // null cannot be casted
-            throw new Error(ErrorCode.INVALID_ARGUMENT, "Cannot cast null to " + destType.Name);
-          else if (obj.GetType() == destType)                   // types match
-            return obj;
-          else if (destType.IsAssignableFrom(obj.GetType()))    // implicit cast will do the job
-            return obj;
-          else if (destType == typeof(JObject))                 // got actual type, but need JObject
-            return new JObject(obj);
-          else if (obj is JObject)                              // got JObject, but need actual type
-            return ((JObject)obj).ToObject(destType);
-          // Special cases
-          else if (obj is long && destType == typeof(int))      // long -> int
-            return Convert.ToInt32((long)obj);
-          else if (obj is long && destType == typeof(uint))     // long -> uint
-            return Convert.ToUInt32((long)obj);
-          else if (obj is double && destType == typeof(float))  // double -> float
-            return (float)(double)obj;
-          else
-            throw new Error(ErrorCode.INVALID_TYPE,
-                            "Cannot cast " + obj.GetType().Name + " to " + destType.Name);
+            // Special cases
+            if (obj == null)                                    // null
+                return null;
+            else if (obj is long && destType == typeof(int))    // long -> int
+                return Convert.ToInt32((long)obj);
+            else if (obj is long && destType == typeof(uint))   // long -> uint
+                return Convert.ToUInt32((long)obj);
+            else if (obj is double && destType == typeof(float))       // double -> float
+                return (float)(double)obj;
+            else if (obj is JObject && destType == typeof(Exception))  // construct exceptions
+                return new Exception(obj.ToString());
+            // General cases
+            else if (obj.GetType() == destType)                 // types match
+                return obj;
+            else if (destType.IsAssignableFrom(obj.GetType()))  // implicit cast will do the job
+                return obj;
+            else if (destType == typeof(JObject))               // got actual type, but need JObject
+                return new JObject(obj);
+            else if (obj is JObject) {                          // got JObject, but need actual type
+                return ((JObject)obj).ToObject(destType);
+            }
+            else                                                // nothing worked - just fail
+                throw new Error(ErrorCode.INVALID_TYPE,
+                    "Cannot cast " + obj.GetType().Name + " to " + destType.Name);
         }
     }
 }
